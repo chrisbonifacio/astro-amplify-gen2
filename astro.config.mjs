669035dd2863a9b03ed27c0555951301491aef07
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-03-28 17:07:42 -0400

    add and use cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 // import awsAmplify from "astro-aws-amplify";
-
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: vercel(),
-  trailingSlash: "always"
+  adapter: cloudflare(),
+  trailingSlash: "always",
 });
