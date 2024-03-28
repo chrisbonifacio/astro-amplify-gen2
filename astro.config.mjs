cd3772c4218a153f7400a9bbc2047b1713c0a5d6
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-03-28 17:19:01 -0400

    fix cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 // import awsAmplify from "astro-aws-amplify";
 // import vercel from "@astrojs/vercel/serverless";
+
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: cloudflare(),
   trailingSlash: "always",
+  adapter: cloudflare()
 });
