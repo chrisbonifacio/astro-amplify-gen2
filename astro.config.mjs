99493e30c0a5e4ca2819c360424d22c83180de65
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-04-04 14:29:12 -0400

    move amplify backend to dev dependencies and deploy to amplify hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
+import awsAmplify from "astro-aws-amplify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
   trailingSlash: "always",
-  adapter: cloudflare(),
+  adapter: awsAmplify(),
 });
